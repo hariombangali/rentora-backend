6947a3bf14cd6633d69016d043acab4d43cd52b4
Hariom Bangali <hariombangali@email.com>
2025-09-11 12:05:43 +0530

    Fix: add axios dependency

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173', // adjust as per your frontend URL
+ origin: process.env.CLIENT_URL || 'http://localhost:5173', // adjust as per your frontend URL
   credentials: true,
 }));
 
