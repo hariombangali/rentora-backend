4a6ea73f6fb3e17b87b1022d857442e191c620f0
Hariom Bangali <hariombangali@email.com>
2025-09-11 16:00:22 +0530

    updated code

--- controllers/PropertyController.js
+++ controllers/PropertyController.js
@@ -30,6 +30,11 @@ const geocodeWithOpenCage = async (address) => {
 // ✅ Post Property
 exports.postProperty = async (req, res) => {
   try {
+
+        console.log("REQ.USER:", req.user);
+    console.log("REQ.BODY:", req.body);
+    console.log("REQ.FILES:", req.files);
+
     const userId = req.user._id;
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User not found" });
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? stack: err.stack,
   });
 };
 module.exports = { errorHandler };
